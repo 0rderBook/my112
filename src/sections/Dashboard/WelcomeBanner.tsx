68afe4a69465eb46861f95c5c43c7b4926c5b89c
0rderBook <163455199+0rderBook@users.noreply.github.com>
2024-05-01 12:54:42 +0300

    Update WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -106,7 +106,7 @@ export function WelcomeBanner() {
         <p></p>
       </div>
       <Buttons>
-        <button onClick={() => window.open('https://www.pump.fun/AoUEdLF2wWDehYfNcS2eWjUZDjn8hYTDaAoQvZsEQX54', '_blank')}>
+        <button onClick={() => window.open('', '_blank')}>
           🎩Buy $CASINO
         </button>
         <button onClick={() => window.open('https://twitter.com/PUMP_FUN_CASINO', '_blank')}>
